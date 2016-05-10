df4787c29082675442e5c81cd7cca4599bbdb46d
keleko34 <gguidero@dataart.com>
2016-05-10 16:42:17 +0200

    implement object observe

--- KB_Model/KB_Model.js
+++ KB_Model/KB_Model.js
@@ -4,6 +4,7 @@ define([],function(){
     var _model = {}
       , _viewmodels = {}
       , _dataListeners = {}
+      , _dataUpdateListeners = {}
       , _changeEvent = function(vm,obj,value,oldValue,scopeString,key,args,action)
         {
           this.stopPropagation = function(){this._stopPropogation = true;};
@@ -46,10 +47,35 @@ define([],function(){
       , _onUpdate = function(vm,obj,value,oldValue,scopeString,key,args,action)
         {
           var e = new _changeEvent(vm,obj,value,oldValue,scopeString,key,args,action);
+          , x;
+          if(_dataUpdateListeners[key] !== undefined)
+          {
+            loop:for(x=0;x<_dataUpdateListeners[key].length;x++)
+            {
+              _dataUpdateListeners[key][x].call({},e); //-- need to bind to parent object
+              if(e._stopPropogation) break loop;
+            }
+          }
+          if(_dataUpdateListeners[scopeString] !== undefined && !e._stopPropogation)
+          {
+            loop:for(x=0;x<_dataUpdateListeners[scopeString].length;x++)
+            {
+              _dataUpdateListeners[scopeString][x].call({},e); //-- need to bind to parent object
+              if(e._stopPropogation) break loop;
+            }
+          }
+          if(e._preventDefault)
+          {
+            return false;
+          }
+          return true;
         }
+      , _saveEnum = ['local','session']
 
     function KB_Model()
     {
+
+      //look at local storage and load, if viewmodel has save, save default to local storage, set also updates local storage
       var keys = Object.keys(_viewmodels)
         , x
         , key = {};
@@ -59,11 +85,11 @@ define([],function(){
         key = _viewmodels[keys[x]];
         key._getData = KB_Model.deepCopy(key._tempData);
 
-        KB_Model.deepInject(key._getData,key._bindData);
+        KB_Model.deepInject(key._getData,key._bindData,keys[x]);
         if(key._model)
         {
           _model[keys[x]] = {};
-          KB_Model.deepInject(key._getData,_model[keys[x]]);
+          KB_Model.deepInject(key._getData,_model[keys[x]],"Model");
         }
       }
     }
@@ -74,7 +100,7 @@ define([],function(){
         return _model;
     }
 
-    KB_Model.ViewModel = function(name,data,model)
+    KB_Model.ViewModel = function(name,data,save,model)
     {
       if(data === undefined && name === undefined)
       {
@@ -84,7 +110,7 @@ define([],function(){
       {
         return _viewmodels[name]._bindData;
       }
-      _viewmodels[name] = {_tempData:data,_getData:{},_bindData:{},_model:!!(model === undefined ? true : model)};
+      _viewmodels[name] = {_tempData:data,_getData:{},_bindData:{},_save:!!(save === undefined ? false : save),_model:!!(model === undefined ? true : model)};
       return KB_Model;
     }
 
@@ -116,13 +142,16 @@ define([],function(){
       return obj2;
     }
 
-    KB_Model.deepInject = function(obj,obj2)
+    KB_Model.deepInject = function(obj,obj2,name,str)
     {
+      str = (str === undefined ? "" : str);
+      name = (name === undefined ? "unknown" : name);
+
       var keys = Object.keys(obj)
         , key
         , x
         , obj2 = (obj2 === undefined ? {} : obj2)
-        , _inject = function(key,value,obj2,str)
+        , _inject = function(key,value)
           {
             if(str.lastIndexOf("[") === (str.length-1))
             {
@@ -137,21 +166,47 @@ define([],function(){
               str += "."+key;
             }
 
-
+            if(value.constructor.toString() === Object.toString() || value.constructor.toString() === Array.toString())
+            {
+              //object.defineProp
+              Object.defineProperty(obj2,key,{
+                get:function(){return obj[key];},
+                set:function(v){
+                  var oldValue = obj2[keys[x]];
+                  if(_onSet(name,obj2,v,oldValue,str,key))
+                  {
+                    obj[key] = v;
+                  }
+                  _onUpdate(name,obj2,v,oldValue,str,key);
+                },
+                enumerable:true,
+                configurable:true
+              })
+              KB_Model.deepInject(key,obj2[keys[x]],str,name);
+            }
+            else
+            {
+              //object.defineProp
+              Object.defineProperty(obj2,key,{
+                get:function(){return obj[key];},
+                set:function(v){
+                  var oldValue = obj2[keys[x]];
+                  if(_onSet(name,obj2,v,oldValue,str,key))
+                  {
+                    obj[key] = v;
+                  }
+                  _onUpdate(name,obj2,v,oldValue,str,key);
+                },
+                enumerable:true,
+                configurable:true
+              })
+            }
           }
 
       for(x=0;x<keys.length;x++)
       {
         key = obj[keys[x]];
-        if(key.constructor.toString() === Object.toString() || key.constructor.toString() === Array.toString())
-        {
-          //object.defineProp
-          KB_Model.deepInject(key,obj2[keys[x]]);
-        }
-        else
-        {
-          //object.defineProp
-        }
+        _inject(keys[x],key);
       }
       return obj2;
     }
@@ -170,6 +225,34 @@ define([],function(){
     {
       var x
         , d = dataListeners[attr];
+      if(d !== undefined)
+      {
+        loop:for(x=0;x<d.length;x++)
+        {
+          if(d[x].toString() === func.toString())
+          {
+            d.splice(x,1);
+            break loop;
+          }
+        }
+      }
+      return KB_Model;
+    }
+
+    KB_Model.addDataUpdateListener = function(attr,func) //name or tree.branch.prop[int] etc.
+    {
+      if(_dataUpdateListeners[attr] === undefined)
+      {
+        _dataUpdateListeners[attr] = [];
+      }
+      _dataUpdateListeners[attr].push(func);
+      return KB_Model;
+    }
+
+    KB_Model.removeDataUpdateListener = function(attr,func)
+    {
+      var x
+        , d = _dataUpdateListeners[attr];
       if(d !== undefined)
       {
         loop:for(x=0;x<d.length;x++)
